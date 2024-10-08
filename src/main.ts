eb5ed9c09b87234e4d56c96ef34c96b7913feaf1
Manish Yadav <manish.yadav@softwareworkshop.net>
2024-10-08 23:45:47 +0530

    first commit

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,16 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT') || 3001;
+  
+  // Enable CORS
+  app.enableCors({
+    origin: configService.get('FRONTEND_URL') || 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  const port = configService.get('BACKEND_PORT') || 3001;
   await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
